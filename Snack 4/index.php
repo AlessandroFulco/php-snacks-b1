77520ef0f27b743f76b62cb3119a09dd6c19b04f
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-06-27 19:43:11 +0200

    Snack 4

---
+++ Snack 4/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$array = [];
+
+while (count($array) <= 15) {
+    $number = rand(1, 100);
+
+    if (!in_array($number, $array)) {
+        $array[] = $number;
+    }
+}
+
+var_dump($array);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
